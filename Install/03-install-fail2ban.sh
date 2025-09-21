1ce833a866f2f576342c7990b438c5e9232f5488
New8ie <mfachmie@gmail.com>
2025-09-21 19:10:59 +0700

    add filter fail2ban

--- Install/03-install-fail2ban.sh
+++ Install/03-install-fail2ban.sh
@@ -42,6 +42,11 @@ apt install -y fail2ban curl
 log "Backup konfigurasi default..."
 cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.conf.bak.$(date +%Y%m%d)
 
+# =============================================================================
+# Install & Konfigurasi Fail2ban Action
+# =============================================================================
+
+
 log "Copy action telegram.conf..."
 cp -f "$DOTFILES_DIR/Fail2Ban/Telegram.conf" /etc/fail2ban/action.d/telegram.conf
 
@@ -52,8 +57,32 @@ log "Copy script telegram_notif..."
 cp -f "$DOTFILES_DIR/Fail2Ban/send_telegram_notif.sh" /etc/fail2ban/script/send_telegram_notif.sh
 chmod +x /etc/fail2ban/script/send_telegram_notif.sh
 
+
 log "Copy action Cloudflare..."
-cp -f "$DOTFILES_DIR/Fail2Ban/Cloudflare.conf" /etc/fail2ban/action.d/cloudflare.conf 
+cp -f "$DOTFILES_DIR/Fail2Ban/Cloudflare.conf" /etc/fail2ban/action.d/cloudflare-logging.conf
+touch /var/log/fail2ban-cloudflare.log
+chown root:root /var/log/fail2ban-cloudflare.log
+chmod 640 /var/log/fail2ban-cloudflare.log
+
+log "Copy action iptables-custom-logging-ipv6..."
+cp -f "$DOTFILES_DIR/Fail2Ban/Iptables-custom-logging-ipv6.conf" /etc/fail2ban/action.d/iptables-custom-logging-ipv6.conf
+touch /var/log/fail2ban-iptables.log
+chown root:root /var/log/fail2ban-iptables.log
+chmod 640 /var/log/fail2ban-iptables.log
+
+
+# =============================================================================
+# Install & Konfigurasi Fail2ban Filter
+# =============================================================================
+
+log "Copy Filter Guacamole..."
+cp -f "$DOTFILES_DIR/Fail2Ban/Guacamole.conf" /etc/fail2ban/filter.d/guacamole.conf
+
+log "Copy Filter Nextcloud..."
+cp -f "$DOTFILES_DIR/Fail2Ban/Nextcloud.conf" /etc/fail2ban/filter.d/nextcloud.conf
+
+log "Copy Filter Immich..."
+cp -f "$DOTFILES_DIR/Fail2Ban/Immich.conf" /etc/fail2ban/filter.d/immich.conf
 
 log "Membuat konfigurasi jail.local..."
 cp -f "$DOTFILES_DIR/Fail2Ban/Jail.conf" /etc/fail2ban/jail.local
